ea23aff8720a8f35e26f6383b530cb942fd6b9be
Chetan K <97790484+chetan0172@users.noreply.github.com>
2025-11-19 21:40:28 +0530

    Refactor SonarQube analysis function
    Refactor SonarQube analysis function to improve readability and add source directory.

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,9 +1,12 @@
-def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeAPI}"){
-      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-  }
-}def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeAPI}"){
-      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-  }
+def call(String SonarQubeAPI, String Projectname, String ProjectKey) {
+
+    withSonarQubeEnv(SonarQubeAPI) {
+        sh """
+            ${env.SONAR_HOME}/bin/sonar-scanner \
+                -Dsonar.projectName=${Projectname} \
+                -Dsonar.projectKey=${ProjectKey} \
+                -Dsonar.sources=. \
+                -X
+        """
+    }
 }
