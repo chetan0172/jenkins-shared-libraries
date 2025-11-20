4761256735f8f68ade58261c5839e44812f19978
Chetan K <97790484+chetan0172@users.noreply.github.com>
2025-11-20 15:23:33 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,35 +1,4 @@
-def call() {
-
-    echo "------ OWASP Dependency Check: Installing Dependencies ------"
-
-    nodejs('Node18') {
-
-        // Install dependencies
-        if (fileExists("backend/package.json")) {
-            dir("backend") { sh "npm install" }
-        }
-
-        if (fileExists("frontend/package.json")) {
-            dir("frontend") { sh "npm install" }
-        }
-
-        if (fileExists("package.json")) {
-            sh "npm install"
-        }
-
-        echo "------ Running OWASP Dependency Check ------"
-
-        dependencyCheck(
-            odcInstallation: 'OWASP',
-            additionalArguments: '''
-                --scan ./ \
-                --disableNodeAudit \
-                --disableAssembly \
-                --disableMSBuild \
-                --format XML
-            '''
-        )
-
-        dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
-    }
+def call(){
+  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
+  dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
